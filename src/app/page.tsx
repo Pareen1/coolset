c54e620fae6065c61c649a64b15d00c0f7bfb54d
Pareen <pareenv@gmail.com>
2025-02-11 17:55:44 +0200

    fix: make it fixed position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { groceryItems } from "@/data/groceryData";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-background py-8 px-16">
+    <div className="flex flex-col items-center  min-h-screen bg-background p-16">
       <div className="w-full">
         <h1 className="">Today&apos;s Groceries</h1>
         <Table data={groceryItems} />
